ebc1e78e6871c573abe64d69dca56697716dd975
natewhit44 <natewhit44@gmail.com>
2016-12-01 13:17:23 -0800

    Pushing changed to the DB connection and sever.js, the results don't display yet but there are no errors.

--- server.js
+++ server.js
@@ -2,14 +2,36 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var app = express();
 var recipeContent = require('./Recipe');
 var port = process.env.PORT || 3000;
 
+// Set up persistant DB connection
+var connection = mysql.createConnection({
+  host: "mysql.cs.orst.edu",
+  user: "cs290_whitlocn",
+  password: "2093",
+  database: "cs290_whitlocn"
+});
+
+// Make the connection to DB, will remain
+// connected until server stops running
+connection.connect(function(err) {
+  if (err) {
+    console.log("== Unable to make connection to MySQL Database.")
+    throw err;
+  }
+});
+
 // Use Handlebars as the view engine for the app.
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+// Parse all request bodies as JSON
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,6 +39,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req,res) {
 	res.render('index-page',{
 		title: "Recipe Repository Home"
+	});
+});
+
+app.get('/mysql', function(req,res){
+	connection.query('SELECT * FROM recipe_name', function(err, rows){
+		if(err){
+			console.log("== Error fectching recipes from DB: ", err);
+			res.status(500).send("Error fetching recipes: " + err);
+		} else{
+			var recipes = [];
+			rows.forEach(function(row){
+				recipes.push({
+					recipe_name: row.recipe_name,
+					recipe_category: row.recipe_category,
+					prep_time: row.prep_time,
+					cook_time: row.cook_time,
+					temp: row.temp,
+					yeild: row.yeild
+				});
+			});
+
+			res.render('index-page',{
+				title: "MySQL Results",
+				recipes: recipes
+			})
+		}
 	});
 });
 
